e07c7e88c4a9156e1cc2225179bb64a22bbade36
tashfeenrao <raotashfeen25@gmail.com>
2020-06-18 02:45:27 +0500

    switching turns for X or O added

--- src/index.js
+++ src/index.js
@@ -17,12 +17,15 @@ const Square = (props) => {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true
       }
     }
     handleClick = (i) => {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares})
+      squares[i] = this.state.xIsNext ? 'X' : 'O'
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext})
     }
     renderSquare(i) {
       return <Square 
