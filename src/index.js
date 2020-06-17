f250681f686b2b31f226626f17cc43973fc9f381
tashfeenrao <raotashfeen25@gmail.com>
2020-06-18 02:33:51 +0500

    made squares component an functional component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+const Square = (props) => {
       return (
         <button 
         className="square"
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
+        onClick={() => props.onClick()}>
+        {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
