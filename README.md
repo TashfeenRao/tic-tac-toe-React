ffb5b40aa38e6237642c08ac0f19899ed5e92112
tashfeenrao <raotashfeen25@gmail.com>
2020-06-19 01:19:11 +0500

    added build files to root

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # tic-tac-toe-React
- This is a game built using React guides. It is a two players game. player with right combinations of X or O wins. 
-#  Roll Dice
+ This is a game built using React guides. It is a two players game. player with right combinations of X or O wins.
 
 <p align="center">
     <img src="final.png">
---
+++ precache-manifest.6388f40f5dc90bddcfed80cd13e07232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae6bce97fc50c54f83687e4638c270b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aacda0cbd07eb8936a66",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a7a743f42dfe253da73c",
+    "url": "/static/js/2.9314f0b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9314f0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacda0cbd07eb8936a66",
+    "url": "/static/js/main.74e74b75.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
